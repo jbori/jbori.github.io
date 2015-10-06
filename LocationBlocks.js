5426e8247816af8943d7906f14da4dab3bdd149b
jbori <josep.bori@gsb.uchicago.edu>
2015-10-06 22:17:08 +0100

    Update

--- LocationBlocks.js
+++ LocationBlocks.js
@@ -21,15 +21,44 @@
         });
     };
 
+	// Ancillary function to post requests to Location Server
+	// Source: http://stackoverflow.com/questions/133925/javascript-post-request-like-a-form-submit
+	function post(path, params, method) {
+		method = method || "post"; // Set method to post by default if not specified.
+
+		// The rest of this code assumes you are not using a library.
+		// It can be made less wordy if you use one.
+		var form = document.createElement("form");
+		form.setAttribute("method", method);
+		form.setAttribute("action", path);
+
+		for(var key in params) {
+			if(params.hasOwnProperty(key)) {
+				var hiddenField = document.createElement("input");
+				hiddenField.setAttribute("type", "hidden");
+				hiddenField.setAttribute("name", key);
+				hiddenField.setAttribute("value", params[key]);
+
+				form.appendChild(hiddenField);
+			 }
+		}
+
+		document.body.appendChild(form);
+		form.submit();
+	}	
+	
     ext.get_latitude = function(location, callback) {
         // Try javascript call to Location Services
-		var xhttp = new XMLHttpRequest();
-		xhttp.open("GET", 'http://api.openweathermap.org/data/2.5/weather?q='+location+'&units=imperial', false);
+		//var xhttp = new XMLHttpRequest();
+		//xhttp.open("GET", 'http://api.openweathermap.org/data/2.5/weather?q='+location+'&units=imperial', false);
 		//xhttp.open("GET", 'http://108.167.143.127/inventiveproject/locationserver.php?service=getlocationdata&username=Marc&field=latitude', false);
-		xhttp.send();
+		//xhttp.send();
 		//document.getElementById("demo").innerHTML = xhttp.responseText;
-		temperature = xhttp.responseText;
-        callback(temperature);
+		//temperature = xhttp.responseText;
+        //callback(temperature);
+		
+		temperature = post('http://108.167.143.127/inventiveproject/locationserver.php', {name: 'service getlocationdata'});
+		callback(temperature);
 		
 		// Make an AJAX call to location services
         /*$.ajax({
