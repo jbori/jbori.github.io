1929557a8f2492d46a61412696df9d9471884a35
jbori <josep.bori@gsb.uchicago.edu>
2015-10-06 23:30:53 +0100

    Update

--- LocationBlocks.js
+++ LocationBlocks.js
@@ -90,11 +90,13 @@
 		
 		// Make an AJAX call to location services
         $.ajax({
-              url: 'http://api.openweathermap.org/data/2.5/weather?q='+location+'&units=imperial',
+              //url: 'http://api.openweathermap.org/data/2.5/weather?q='+location+'&units=imperial',
+			  url: 'http://108.167.143.127/inventiveproject/locationserver.php?service=getlocationdata&username=Marc&field=latitudre',
               dataType: 'jsonp',
-              success: function( weather_data ) {
+              success: function( location_data ) {
                   // Got the data - parse it and return the temperature
-                  temperature = weather_data['main']['temp'];
+                  //temperature = location_data['main']['temp'];
+				  temperature = location_data;
                   callback(temperature);
               }
         });
