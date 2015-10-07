c2eb4899e9e60325306e6032dbdaf2e2e2dab1a7
jbori <josep.bori@gsb.uchicago.edu>
2015-10-07 22:27:38 +0100

    Update

--- LocationBlocks.js
+++ LocationBlocks.js
@@ -8,7 +8,7 @@
         return {status: 2, msg: 'Ready'};
     };
 
-    ext.get_temp = function(location, callback) {
+    /*ext.get_temp = function(location, callback) {
         // Make an AJAX call to the Open Weather Maps API
         $.ajax({
               url: 'http://api.openweathermap.org/data/2.5/weather?q='+location+'&units=imperial',
@@ -19,33 +19,8 @@
                   callback(temperature);
               }
         });
-    };
+    };*/
 
-	// Ancillary function to post requests to Location Server
-	// Source: http://stackoverflow.com/questions/133925/javascript-post-request-like-a-form-submit
-	/*function post(path, params, method) {
-		method = method || "post"; // Set method to post by default if not specified.
-
-		// The rest of this code assumes you are not using a library.
-		// It can be made less wordy if you use one.
-		var form = document.createElement("form");
-		form.setAttribute("method", method);
-		form.setAttribute("action", path);
-
-		for(var key in params) {
-			if(params.hasOwnProperty(key)) {
-				var hiddenField = document.createElement("input");
-				hiddenField.setAttribute("type", "hidden");
-				hiddenField.setAttribute("name", key);
-				hiddenField.setAttribute("value", params[key]);
-
-				form.appendChild(hiddenField);
-			 }
-		}
-
-		document.body.appendChild(form);
-		form.submit();
-	}*/	
 	
     ext.get_latitude = function(username, callback) {
 		// Obtain location data for username from Location Services Broker
@@ -118,13 +93,37 @@
     };
 	
 	
+    ext.get_accuracy = function(username, callback) {
+		// Obtain location data for username from Location Services Broker
+		var xhttp = new XMLHttpRequest();
+		var postdata= "service=getlocationdata&username="+username;
+
+		// Open HTTP request
+		xhttp.open("POST", "http://108.167.143.127/inventiveproject/locationserver.php", true);
+
+		//Send the proper header information along with the request
+		xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xhttp.setRequestHeader("Content-length", postdata.length);
+		xhttp.setRequestHeader("Connection", "close");
+
+		xhttp.onreadystatechange = function() {//Call a function when the state changes.
+		   if(xhttp.readyState == 4 && xhttp.status == 200) {
+			  var location_data = JSON.parse(xhttp.responseText);
+			  callback(location_data.accuracy);
+		   }
+		}
+		xhttp.send(postdata);	
+    };
+
+
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            ['R', 'current temperature in city %s', 'get_temp', 'Boston, MA'],
+            //['R', 'current temperature in city %s', 'get_temp', 'Boston, MA'],
 			['R', '%s s latitude', 'get_latitude', 'Marc'],
-			['R', '%s s longitude', 'get_longitude', 'Boston, MA'],
-			['R', '%s s altitude', 'get_altitude', 'Boston, MA'],
+			['R', '%s s longitude', 'get_longitude', 'Marc'],
+			['R', '%s s altitude', 'get_altitude', 'Marc'],
+			['R', '%s s altitude', 'get_altitude', 'Marc'],
         ]
     };
 
